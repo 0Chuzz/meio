6b1e70f585e6e5f42d2c74abd1288011c855226f
chuzz <chuzz.chuzz@gmail.com>
2013-02-12 21:34:27 +0100

    parser stub

---
+++ include/parser.h
@@ -0,0 +1,8 @@
+#ifndef __PARSER_H
+#define __PARSER_H
+#include "lexer.h"
+
+typedef struct _iomessage{
+}  iomessage_t;
+
+#endif
---
+++ src/parser.c
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include "parser.h"
+
+iomessage_t* parse(char *code){
+    return NULL;
+}
