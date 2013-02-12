77b0d0b1bbf02255948991213ab2d92e531dbbb2
chuzz <chuzz.chuzz@gmail.com>
2013-02-12 22:01:51 +0100

    main loop in parser

--- include/parser.h
+++ include/parser.h
@@ -3,6 +3,11 @@
 #include "lexer.h"
 
 typedef struct _iomessage{
+    char *name;
+    struct _iomessage **arguments;
+    struct _iomessage *next;
 }  iomessage_t;
 
+
+iomessage_t *parse(char *code);
 #endif
--- src/parser.c
+++ src/parser.c
@@ -2,5 +2,14 @@
 #include "parser.h"
 
 iomessage_t* parse(char *code){
-    return NULL;
+    token_t currtoken;
+    iomessage_t *message = NULL;
+    do {
+        currtoken = scan(code);
+        switch(currtoken.type) {
+        }
+        code += currtoken.size;
+    } while ( currtoken.type != ERROR && currtoken.type != END);
+    
+    return message;
 }
